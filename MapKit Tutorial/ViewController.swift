2893d176dd738ed8fa70cc3c81a161e2daaa292f
jakkujakku <jakkujakku98@gmail.com>
2022-11-21 01:21:49 +0900

    Decoding GeoJSON with MKGeoJSONDecoder

--- MapKit Tutorial/ViewController.swift
+++ MapKit Tutorial/ViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 import MapKit
 
+
 class ViewController: UIViewController {
     
     @IBOutlet private var mapView: MKMapView!
     
-    let initialLocation = CLLocation(latitude: 37.5866, longitude: 126.9748)
+    // Set initial location in Honolulu
+    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
+
         mapView.centerToLocation(initialLocation)
+        
+        let oahuCenter = CLLocation(latitude: 21.4765, longitude: -157.9647)
+        let region = MKCoordinateRegion(
+          center: oahuCenter.coordinate,
+          latitudinalMeters: 50000,
+          longitudinalMeters: 60000)
+        mapView.setCameraBoundary(
+          MKMapView.CameraBoundary(coordinateRegion: region),
+          animated: true)
+        
+        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+        mapView.setCameraZoomRange(zoomRange, animated: true)
+
+        // Show artwork on map
+        let artwork = Artwork(
+          title: "King David Kalakaua",
+          locationName: "Waikiki Gateway Park",
+          discipline: "Sculpture",
+          coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        mapView.addAnnotation(artwork)
+
     }
 
 
@@ -33,3 +60,50 @@ private extension MKMapView {
     setRegion(coordinateRegion, animated: true)
   }
 }
+
+extension ViewController: MKMapViewDelegate {
+  // 1
+  func mapView(
+    _ mapView: MKMapView,
+    viewFor annotation: MKAnnotation
+  ) -> MKAnnotationView? {
+      // 2
+      guard let annotation = annotation as? Artwork else {
+          return nil
+      }
+      // 3
+      let identifier = "artwork"
+      var view: MKMarkerAnnotationView
+      // 4
+      if let dequeuedView = mapView.dequeueReusableAnnotationView(
+        withIdentifier: identifier) as? MKMarkerAnnotationView {
+          dequeuedView.annotation = annotation
+          view = dequeuedView
+      } else {
+          // 5
+          view = MKMarkerAnnotationView(
+            annotation: annotation,
+            reuseIdentifier: identifier)
+          view.canShowCallout = true
+          view.calloutOffset = CGPoint(x: -5, y: 5)
+          view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+      }
+      return view
+  }
+    func mapView(
+      _ mapView: MKMapView,
+      annotationView view: MKAnnotationView,
+      calloutAccessoryControlTapped control: UIControl
+    ) {
+      guard let artwork = view.annotation as? Artwork else {
+        return
+      }
+
+      let launchOptions = [
+        MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+      ]
+      artwork.mapItem?.openInMaps(launchOptions: launchOptions)
+    }
+    
+}
+
